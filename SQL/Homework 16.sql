fa8770d2f2c4e61c485e303f3131c6e84e22ad08
Maria <masha.tupichenko71@gmail.com>
2024-11-28 10:37:44 +0300

    Add files via upload

---
+++ SQL/Homework 16.sql
@@ -0,0 +1,36 @@
+/*Задание 1
+SELECT * 
+FROM [Sales].[Customer]
+WHERE TerritoryID > 5
+ORDER BY TerritoryID DESC*/
+
+/*Задание 2
+SELECT TOP 10 [ProductID],[Name], [ListPrice]
+FROM [Production].[Product]
+ORDER BY [ListPrice] DESC*/
+
+/* Задание 3
+SELECT [BusinessEntityID], [FirstName], [LastName] 
+FROM Person.Person
+WHERE [LastName] LIKE 'M%'*/
+
+/*Задание 3=4
+CREATE TABLE TopCustomers (
+	CustomerID INT PRIMARY KEY, 
+	FirstName NVARCHAR(30), 
+	LastName NVARCHAR(30)
+)
+
+INSERT INTO TopCustomers (CustomerID, FirstName, LastName)
+VALUES 
+	(1, 'Masha', 'Tupichenko'),
+	(2, 'Denis', 'Ivaniv'),
+	(3, 'Anna', 'Petrova')
+SELECT * FROM TopCustomers
+ORDER BY LastName ASC*/
+
+/* Задание 5
+SELECT [TerritoryID], AVG(TotalDue) AS [AverageOrderAmount] FROM [Sales].[SalesOrderHeader]
+WHERE [ShipDate] > [OrderDate]
+GROUP BY [TerritoryID]*/
+
